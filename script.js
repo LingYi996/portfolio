070aefbfe3441b85b786cc4958bb406f0c12622e
饺子君 <Mrwolf@Ling-4.local>
2023-05-18 01:14:52 +0800

    iteration1

---
+++ script.js
@@ -0,0 +1,12 @@
+const svg = document.getElementById("svg-canvas");
+
+window.addEventListener("resize", resizeSvg);
+
+function resizeSvg(){
+    let bbox = svg.getBoundingClientRect();
+    svg.setAttribute("viewBox", `0 0 ${bbox.width} ${bbox.height}`);
+    
+    for(let circle of svg.children){
+        circle.setAttribute('r',  Math.min(bbox.width, bbox.height) * 0.1);
+    }
+}
